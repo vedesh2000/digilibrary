60462bb7e855e4f6e4cb6b62ad278f000e72923a
vedesh2000 <vedeshkompella@gmail.com>
2023-03-23 19:46:25 +0530

    Completed basic back end

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const routes = express.Router();
+const Book = require("../models/book")
 
-routes.get('/' , (req, res) => {
-    res.render('index')
+routes.get('/' , async (req, res) => {
+    let books
+    try{
+        books = await Book.find().sort({ createdAt : 'desc'}).limit(10).exec()
+    }
+    catch{
+        books = []
+    }
+    res.render('index' , {books : books})
 })
 module.exports = routes
