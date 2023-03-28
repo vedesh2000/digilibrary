80d4046cc3893254e64ae17da7a08ae3bc24511e
vedesh2000 <vedeshkompella@gmail.com>
2023-03-28 12:24:00 +0530

    changed image upload from multer to filepond

--- models/book.js
+++ models/book.js
@@ -22,7 +22,11 @@ const bookSchema = mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType: {
         type: String,
         required: true
     },
@@ -34,10 +38,9 @@ const bookSchema = mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function() {
-    if(this.coverImageName != null){
-        return path.join('/', coverImageBasePath, this.coverImageName)
+    if(this.coverImage != null && this.coverImageType != null){
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
 })
 
 module.exports = mongoose.model('Book' , bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,15 @@
 const express = require("express")
 const router = express.Router()
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
+// const multer = require('multer')
 const Book = require("../models/book")
-const uploadPath = path.join('public', Book.coverImageBasePath)
 const  Author = require("../models/author")
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg']
-const upload = multer({
-    dest: uploadPath,
-    fileFilter: (req, file, callback) =>{
-        callback(null , imageMimeTypes.includes(file.mimetype))
-    }
-})
+// const upload = multer({
+//     dest: uploadPath,
+//     fileFilter: (req, file, callback) =>{
+//         callback(null , imageMimeTypes.includes(file.mimetype))
+//     }
+// })
 
 //all authors route
 router.get('/',async (req,res)=>{
@@ -41,33 +38,32 @@ router.get('/new',async(req,res)=>{
 });
 
 //new author route
-router.post('/',upload.single('cover'), async (req,res)=>{
-    const fileName = req.file != null ? req.file.filename  : req.body.title + " By " + req.body.author
+router.post('/', async (req,res)=>{
     const book = new Book({
         title: req.body.title,
         author:req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
-        coverImageName: fileName,
         description: req.body.description
     })
+    saveCover(book , req.body.cover)
     try{
         const newBook = await book.save()
         res.redirect('books')
     }
     catch(error){
         console.log(error)
-        if(book.coverImageName != null){
-            removeBookCover(book.coverImageName)
-        }
+        // if(book.coverImageName != null){
+        //     removeBookCover(book.coverImageName)
+        // }
         renderNewPage(res, book, true)
     }
 })
-function removeBookCover(fileName){
-    fs.unlink(path.join(uploadPath , fileName) , err => {
-        if(err) console.log(err);
-    })
-}
+// function removeBookCover(fileName){
+//     fs.unlink(path.join(uploadPath , fileName) , err => {
+//         if(err) console.log(err);
+//     })
+// }
 async function renderNewPage(res, book, hasError = false){
     try{
         const authors = await Author.find({})
@@ -82,6 +78,14 @@ async function renderNewPage(res, book, hasError = false){
         res.redirect('/books')
     }
 }
-
+function saveCover(book , coverEncoded){
+    if(coverEncoded == null) return
+    const cover = JSON.parse(coverEncoded)
+    if(cover != null && imageMimeTypes.includes(cover.type)){
+        book.coverImage = new Buffer.from(cover.data, 'base64')
+        book.coverImageType = cover.type
+    }
+    
+}
 module.exports = router
 
