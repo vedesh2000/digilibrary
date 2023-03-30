8ee1a9a43cebc027d3befababc585804ad1ce2b9
vedesh2000 <vedeshkompella@gmail.com>
2023-03-30 13:49:07 +0530

    Finished Author Routes

--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const Book = require("./book")
 const authorSchema = mongoose.Schema({
     name: {
         type: String,
@@ -7,4 +7,18 @@ const authorSchema = mongoose.Schema({
     }
 })
 
+authorSchema.pre('deleteOne', { document: true }, function(next)  {
+    Book.find({author: this.id})
+    .then((books) => {
+        // console.log(books.length)
+        if(books.length > 0){
+            next(new Error('This Author still has books'))
+        }else{
+            next()
+        }
+    })
+    .catch((err) => {
+            next(err)
+    });
+})
 module.exports = mongoose.model('Author' , authorSchema)
